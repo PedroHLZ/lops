4ee0a3faeb762a7cd014cd362b7fdcc3aa8e8921
Pedro H. L. Queiroz <123014853+PedroHLZ@users.noreply.github.com>
2023-05-30 23:16:19 -0300

    responsive 768 e 411px

--- js/script.js
+++ js/script.js
@@ -35,35 +35,6 @@ $(document).ready(function () {
     });
 });
 
-
-//    trocar cor    //
-const button1 = document.getElementById('change-colors-button-1');
-const button2 = document.getElementById('change-colors-button-2');
-const button3 = document.getElementById('change-colors-button-3');
-
-button1.addEventListener('click', () => {
-    document.documentElement.style.setProperty('--bg-color', '#000c20');
-    document.documentElement.style.setProperty('--second-bg-color', '#112e42');
-    document.documentElement.style.setProperty('--text-color', '#ededed');
-    document.documentElement.style.setProperty('--main-color', '#00abf0');
-});
-
-button2.addEventListener('click', () => {
-    document.documentElement.style.setProperty('--bg-color', '#FFF4EE');
-    document.documentElement.style.setProperty('--second-bg-color', '#BA8475');
-    document.documentElement.style.setProperty('--text-color', '#281713');
-    document.documentElement.style.setProperty('--main-color', '#ff0000');
-});
-
-button3.addEventListener('click', () => {
-    document.documentElement.style.setProperty('--bg-color', '#3F4A3C');
-    document.documentElement.style.setProperty('--second-bg-color', '#A3AF9E');
-    document.documentElement.style.setProperty('--text-color', '#000000');
-    document.documentElement.style.setProperty('--main-color', '#00FF00');
-});
-
-
-
 //    header active nav    //
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -84,9 +55,7 @@ window.addEventListener('scroll', () => {
                     link.classList.remove("active");
                 }
             });
-            sec.classList.add("show-animate");
-        } else {
-            sec.classList.remove("show-animate");
+            
         }
     });
 });
@@ -109,11 +78,3 @@ window.addEventListener('scroll', () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 });
-//    modal    //
-function openModal(modalId) {
-    document.getElementById(modalId).style.display = "block";
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = "none";
-}
